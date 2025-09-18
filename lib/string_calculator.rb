5cc27f308c759b65386d7cc9d186debfe1c5ee5a
Anuja24 <anujaraut24@gmail.com>
2025-09-18 12:13:31 +0530

    adds two numbers separated by comma

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,7 @@
 class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
-    return numbers.to_i
+    numbers = numbers.split(",").map(&:to_i)
+    numbers.sum
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,9 +5,11 @@ RSpec.describe StringCalculator do
     it "returns 0 for an empty string" do
       expect(StringCalculator.new.add("")).to eq(0)
     end
-
     it "returns the number for a single number" do
       expect(StringCalculator.new.add("1")).to eq(1)
+    end
+    it "adds two numbers separated by comma" do
+      expect(StringCalculator.new.add("1,5")).to eq(6)
     end
   end
 end
