d1ae97c2b70068e1837f5200bf2b4f71472edafd
Anuja24 <anujaraut24@gmail.com>
2025-09-19 13:44:01 +0530

    refactor the test

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,27 +1,29 @@
 require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
   describe "add" do
     it "returns 0 for an empty string" do
-      expect(StringCalculator.new.add("")).to eq(0)
+      expect(calculator.add("")).to eq(0)
     end
     it "returns the number for a single number" do
-      expect(StringCalculator.new.add("1")).to eq(1)
+      expect(calculator.add("1")).to eq(1)
     end
     it "adds two numbers separated by comma" do
-      expect(StringCalculator.new.add("1,5")).to eq(6)
+      expect(calculator.add("1,5")).to eq(6)
     end
     it "adds any amount of numbers separated by comma" do
-      expect(StringCalculator.new.add("1,2,3,4,5")).to eq(15)
+      expect(calculator.add("1,2,3,4,5")).to eq(15)
     end
     it "handles newlines as separators in addition to commas" do
-      expect(StringCalculator.new.add("1\n2,3")).to eq(6)
+      expect(calculator.add("1\n2,3")).to eq(6)
     end
     it "supports custom single-character delimiter" do
-      expect(StringCalculator.new.add("//;\n1;2")).to eq(3)
+      expect(calculator.add("//;\n1;2")).to eq(3)
     end
     it "raises an exception when there are negative numbers" do
-      expect { StringCalculator.new.add("1,-2,3,-4") }.to raise_error(RuntimeError, "negative numbers not allowed -2,-4")
+      expect { calculator.add("1,-2,3,-4") }.to raise_error(RuntimeError, "negative numbers not allowed -2,-4")
     end
   end
 end
